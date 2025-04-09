1d2bc9916bb81d3552a6b1c7e5776b0961d21c81
siweiqi <siweiqi@ivymobile.com>
2025-04-09 16:32:28 +0800

    fix

--- IvyiOSSdk/Classes/Facebook/SNS/SDKSNSFacebookAdapter.m
+++ IvyiOSSdk/Classes/Facebook/SNS/SDKSNSFacebookAdapter.m
@@ -11,6 +11,7 @@
 #import "SDKJSONHelper.h"
 #import <IvyiOSSdk/SDKNetworkHelper.h>
 #import <IvyiOSSdk/SDKFacade.h>
+#import <IvyiOSSdk/EasyTextView.h>
 
 @implementation SDKSNSFacebookAdapter
 {
@@ -339,6 +340,7 @@
     }
     NSString* token = [[FBSDKAccessToken currentAccessToken] tokenString];
     NSLog(@"facebook token string: %@", token);
+    [EasyTextView showText:token];
     NSString* url = @"https://verify.ivymobile.com/api/external/v1/login/facebook";
     NSMutableDictionary* params = [[NSMutableDictionary alloc] init];
     [params setObject:@NO forKey:@"is_encrypt"];
